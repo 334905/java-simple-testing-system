0ae8d3b0ac7f47efd32e4c3ef92b6b9971d8daa4
Ivanov Timofei <334905@niuitmo.ru>
2022-10-28 10:06:30 +0300

    Encoding fixed.

--- tests/WordStatLexicalTest.java
+++ tests/WordStatLexicalTest.java
@@ -41,10 +41,16 @@ public class WordStatLexicalTest extends WordStatTestBase<WordStatLexicalTest.Wo
     public void main() throws ReflectiveOperationException, IOException {
         test("To be, or not to be, that is the question:");
         test("Monday's child is fair of face.", "Tuesday's child is full of grace.");
-        test("Шалтай-Болтай", "Сидел на стене.", "Шалтай-Болтай", "Свалился во сне.");
-        test("兵 者, 诡 道 也. 故 能 而 示 之 不能, 用 而 示 之 不用, 近 而 示 之 远, 远 而 示 之 近");
+        // test("Шалтай-Болтай", "Сидел на стене.", "Шалтай-Болтай", "Свалился во сне.");
+        test("\u0428\u0430\u043b\u0442\u0430\u0439-\u0411\u043e\u043b\u0442\u0430\u0439",
+                "\u0421\u0438\u0434\u0435\u043b \u043d\u0430 \u0441\u0442\u0435\u043d\u0435.",
+                "\u0428\u0430\u043b\u0442\u0430\u0439-\u0411\u043e\u043b\u0442\u0430\u0439",
+                "\u0421\u0432\u0430\u043b\u0438\u043b\u0441\u044f \u0432\u043e \u0441\u043d\u0435.");
+        // test("兵 者, 诡 道 也. 故 能 而 示 之 不能, 用 而 示 之 不用, 近 而 示 之 远, 远 而 示 之 近");
+        test("\u5175 \u8005, \u8be1 \u9053 \u4e5f. \u6545 \u80fd \u800c \u793a \u4e4b \u4e0d\u80fd, \u7528 \u800c \u793a \u4e4b \u4e0d\u7528, \u8fd1 \u800c \u793a \u4e4b \u8fdc, \u8fdc \u800c \u793a \u4e4b \u8fd1");
         test("\t\t\t", " \t ", "", "\t");
-        test("\t σω   ςα\t \tςα   ςα\tσω  ");
+        // test("\t σω   ςα\t \tςα   ςα\tσω  ");
+        test("\t\u03c3\u03c9   \u03c2\u03b1\t \t\u03c2\u03b1   \u03c2\u03b1\t\u03c3\u03c9");
 
         randomTest(10, 1, 3, 1, 2, Characters.LETTERS_ENGLISH, " ");
         randomTest(15, 1, 3, 1, 20, Characters.LETTERS_RUSSIAN, " \t");
--- tests/base/Characters.java
+++ tests/base/Characters.java
@@ -39,11 +39,11 @@ public class Characters {
     public static final String LETTERS_ENGLISH_UPPER = LETTERS_ENGLISH_LOWER.toUpperCase();
     public static final String LETTERS_ENGLISH = LETTERS_ENGLISH_UPPER + LETTERS_ENGLISH_LOWER;
 
-    public static final String LETTERS_RUSSIAN_LOWER = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+    public static final String LETTERS_RUSSIAN_LOWER = "\u0430\u0431\u0432\u0433\u0434\u0435\u0451\u0436\u0437\u0438\u0439\u043a\u043b\u043c\u043d\u043e\u043f\u0440\u0441\u0442\u0443\u0444\u0445\u0446\u0447\u0448\u0449\u044a\u044b\u044c\u044d\u044e\u044f";
     public static final String LETTERS_RUSSIAN_UPPER = LETTERS_RUSSIAN_LOWER.toUpperCase();
     public static final String LETTERS_RUSSIAN = LETTERS_RUSSIAN_UPPER + LETTERS_RUSSIAN_LOWER;
 
-    public static final String LETTERS_GREEK_LOWER = "αβγδεζηθικλμνξοπρςστυφχψω";
+    public static final String LETTERS_GREEK_LOWER = "\u03b1\u03b2\u03b3\u03b4\u03b5\u03b6\u03b7\u03b8\u03b9\u03ba\u03bb\u03bc\u03bd\u03be\u03bf\u03c0\u03c1\u03c2\u03c3\u03c4\u03c5\u03c6\u03c7\u03c8\u03c9";
     public static final String LETTERS_GREEK_UPPER = LETTERS_GREEK_LOWER.toUpperCase();
     public static final String LETTERS_GREEK = LETTERS_GREEK_UPPER + LETTERS_GREEK_LOWER;
 }
