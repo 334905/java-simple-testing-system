96a612ba6c3addb49ac67e7252306d20b6aead0f
Ivanov Timofei <334905@niuitmo.ru>
2022-10-29 09:41:22 +0300

    More record to class.

--- tests/WordStatLexicalTest.java
+++ tests/WordStatLexicalTest.java
@@ -16,7 +16,44 @@ public class WordStatLexicalTest extends WordStatTestBase<WordStatLexicalTest.Wo
         super(new WordStatLexicalTester());
     }
 
-    protected static record WordAndCount(String word, long count) {
+    protected static final class WordAndCount {
+        private final String word;
+        private final long count;
+
+        private WordAndCount(String word, long count) {
+            this.word = word;
+            this.count = count;
+        }
+
+        public String word() {
+            return word;
+        }
+
+        public long count() {
+            return count;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj == this) return true;
+            if (obj == null || obj.getClass() != this.getClass()) return false;
+            var that = (WordAndCount) obj;
+            return Objects.equals(this.word, that.word) &&
+                    this.count == that.count;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(word, count);
+        }
+
+        @Override
+        public String toString() {
+            return "WordAndCount[" +
+                    "word=" + word + ", " +
+                    "count=" + count + ']';
+        }
+
     }
 
     private static class WordStatLexicalTester extends WordStatTesterBase<WordAndCount> {
