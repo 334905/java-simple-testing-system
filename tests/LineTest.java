a193785c38637ee91dddccb79396d5b8cc3f4756
Ivanov Timofei <334905@niuitmo.ru>
2022-11-13 07:10:35 +0300

    More `Line` tests.

--- tests/LineTest.java
+++ tests/LineTest.java
@@ -1,8 +1,6 @@
 import line.LineTester;
 import line.Point;
 
-import java.util.Objects;
-
 public class LineTest {
     private static void expectTrue(final boolean cond, final String message) {
         if (!cond)
@@ -13,14 +11,20 @@ public class LineTest {
         expectTrue(!cond, message);
     }
 
-    private static void expectEqual(final Object first, final Object second, final String message) {
-        expectTrue(Objects.equals(first, second), message);
+    private static void expectNearlyEqual(final Point first, final Point second, final String message) {
+        final double EPS = 1e-7;
+        expectTrue(Math.abs(first.x - second.x) < EPS && Math.abs(first.y - second.y) < EPS, message);
     }
 
     public static void main(String[] args) throws NoSuchFieldException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException {
         final LineTester tester = new LineTester();
-        expectEqual(tester.intersect(tester.OX, tester.OY), new Point(0, 0), "OX and OY should intersect at (0;0)");
-        expectEqual(tester.intersect(tester.OY, tester.OX), new Point(0, 0), "OY and OX should intersect at (0;0)");
+
+        expectTrue(tester.contains(tester.OX, new Point(1.5, 0)), "OX should contain at (1.5;0)");
+        expectTrue(tester.contains(tester.OY, new Point(0, -.21)), "OY should contain at (0;-0.21)");
+        expectFalse(tester.contains(tester.OX, new Point(1, 1)), "OX should not contain at (1;1)");
+        expectFalse(tester.contains(tester.OY, new Point(-7.2, 1)), "OY should not contain at (-7.2;1)");
+        expectNearlyEqual(tester.intersect(tester.OX, tester.OY), new Point(0, 0), "OX and OY should intersect at (0;0)");
+        expectNearlyEqual(tester.intersect(tester.OY, tester.OX), new Point(0, 0), "OY and OX should intersect at (0;0)");
         expectTrue(tester.parallelTo(tester.OX, tester.OX), "OX should be parallel to itself");
         expectTrue(tester.parallelTo(tester.OY, tester.OY), "OY should be parallel to itself");
         expectFalse(tester.parallelTo(tester.OX, tester.OY), "OX should not be parallel to OY");
@@ -72,11 +76,35 @@ public class LineTest {
         expectTrue(tester.perpendicularTo(xEqualsOne, yEqualsNegOne),
                 "(1;0)---(1;1) should be perpendicular to (0;-1)---(1;-1)");
 
-        expectEqual(tester.intersect(xEqualsOne, yEqualsNegOne), new Point(1, -1),
+        expectNearlyEqual(tester.intersect(xEqualsOne, yEqualsNegOne), new Point(1, -1),
                 "(1;0)---(1;1) and (0;-1)---(1;-1) should intersect at (1;-1)");
-        expectEqual(tester.intersect(xEqualsNegOne, tester.OX), new Point(-1, 0),
+        expectNearlyEqual(tester.intersect(xEqualsNegOne, tester.OX), new Point(-1, 0),
                 "(-1;0)---(-1;1) and OX should intersect at (1;0)");
-        expectEqual(tester.intersect(tester.OY, yEqualsOne), new Point(0, 1),
+        expectNearlyEqual(tester.intersect(tester.OY, yEqualsOne), new Point(0, 1),
                 "OY and (0;1)---(1;1) should intersect at (0;1)");
+
+        expectTrue(tester.contains(xEqualsOne, new Point(1, -2.5)),
+                "(1;0)---(1;1) should contain (1;-2.5)");
+        expectTrue(tester.contains(xEqualsNegOne, new Point(-1, .25)),
+                "(-1;0)---(-1;1) should contain (-1;0.25)");
+        expectTrue(tester.contains(yEqualsOne, new Point(0, 1)),
+                "(0;1)---(1;1) should contain (0;1)");
+
+        final Object line1 = tester.createOfTwoPoints(new Point(0, 1), new Point(2, 4)); //1.5x+1
+        final Object line2 = tester.createOfTwoPoints(new Point(0, -3), new Point(-3, -1)); //-2/3x-3
+        expectTrue(tester.contains(line2, new Point(-4.5, 0)),
+                "(0;-3)---(-3;-1) should contain (-4.5;0)");
+        expectTrue(tester.contains(line1, new Point(-2, -2)),
+                "(0;1)---(2;4) should contain (-2;-2)");
+        expectTrue(tester.perpendicularTo(line1, line2),
+                "(0;1)---(2;4) should be perpendicular to (0;-3)---(-3;-1)");
+        expectTrue(tester.perpendicularTo(line2, line1),
+                "(0;-3)---(-3;-1) should be perpendicular to (0;1)---(2;4)");
+        expectTrue(tester.parallelTo(line1, tester.createOfTwoPoints(new Point(0, 0), new Point(2, 3))),
+                "(0;1)---(2;4) should be parallel to (0;0)---(2;3)");
+        expectFalse(tester.equalTo(line1, tester.createOfTwoPoints(new Point(0, 0), new Point(2, 3))),
+                "(0;1)---(2;4) should not be equal to (0;0)---(2;3)");
+        expectNearlyEqual(tester.intersect(line1, line2), new Point(-24.0/13, -23.0/13),
+                "(0;1)---(2;4) and (0;-3)---(-3;-1) should intersect at (-24/13;-23/13)");
     }
 }
--- tests/line/Point.java
+++ tests/line/Point.java
@@ -10,22 +10,4 @@ public class Point {
         this.x = x;
         this.y = y;
     }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null || getClass() != obj.getClass()) {
-            return false;
-        }
-        final Point point = (Point) obj;
-        return (x == point.x || Double.isNaN(x) && Double.isNaN(point.x)) &&
-                (y == point.y || Double.isNaN(y) && Double.isNaN(point.y));
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
 }
