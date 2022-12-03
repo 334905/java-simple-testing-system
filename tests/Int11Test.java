85e016671b073c0b8f0266af89782d01dd31b118
Ivanov Timofei <334905@niuitmo.ru>
2022-12-03 07:21:21 +0300

    Int11 added.

--- tests/Int11Test.java
+++ tests/Int11Test.java
@@ -1,10 +1,10 @@
 import int_n.IntNTestBase;
 import int_n.IntNTester;
 
-public class Int12Test extends IntNTestBase {
+public class Int11Test extends IntNTestBase {
     public static void main(String[] args)
             throws NoSuchFieldException, ClassNotFoundException, NoSuchMethodException, IllegalAccessException, InstantiationException {
-        final IntNTester tester = new IntNTester("Int12");
+        final IntNTester tester = new IntNTester("Int11");
 
         expectEqual(tester, (short) 0, (short) 0, "0", "0");
         expectGreater(tester, (short) 1, (short) 0, "1", "0");
@@ -22,11 +22,11 @@ public class Int12Test extends IntNTestBase {
                 tester.add(tester.create((short) -1), tester.create((short) 1)),
                 tester.create((short) 0), "(-1 + 1)", "0");
         expectEqual(tester,
-                tester.add(tester.create((short) 0), tester.create((short) 1944)),
-                tester.create((short) 1944), "(0 + 1944)", "1944");
+                tester.add(tester.create((short) 0), tester.create((short) 944)),
+                tester.create((short) 944), "(0 + 944)", "944");
         expectEqual(tester,
-                tester.add(tester.create((short) 1000), tester.create((short) 1000)),
-                tester.create((short) 2000), "(1000 + 1000)", "2000");
+                tester.add(tester.create((short) 500), tester.create((short) 500)),
+                tester.create((short) 1000), "(500 + 500)", "1000");
         expectEqual(tester,
                 tester.add(tester.create((short) -1000), tester.create((short) 785)),
                 tester.create((short) -215), "(-1000 + 785)", "-215");
@@ -52,19 +52,20 @@ public class Int12Test extends IntNTestBase {
                 tester.multiply(tester.create((short) 5), tester.create((short) -140)),
                 tester.create((short) -700), "(5 * -140)", "-700");
         expectEqual(tester,
-                tester.multiply(tester.create((short) -785), tester.create((short) -2)),
-                tester.create((short) 1570), "(-785 * -2)", "1570");
+                tester.multiply(tester.create((short) -324), tester.create((short) -3)),
+                tester.create((short) 972), "(-324 * -3)", "972");
 
-        expectEqual(tester, tester.create((short) -2048), tester.MIN_VALUE, "-2048", "MIN_VALUE");
-        expectEqual(tester, tester.MAX_VALUE, tester.create((short) 2047), "MAX_VALUE", "2047");
-        expectEqual(tester, (short) -2048, (short) 2048, "-2048", "2048");
+        expectEqual(tester, tester.create((short) -1024), tester.MIN_VALUE, "-1024", "MIN_VALUE");
+        expectEqual(tester, tester.MAX_VALUE, tester.create((short) 1023), "MAX_VALUE", "1023");
+        expectEqual(tester, (short) -1024, (short) 1024, "-1024", "1024");
+        expectEqual(tester, (short) 0, (short) 2048, "0", "2048");
         expectEqual(tester, (short) 4096, (short) 0, "4096", "0");
         expectEqual(tester,
                 tester.subtract(tester.MIN_VALUE, tester.create((short) 1)),
                 tester.MAX_VALUE, "(MIN_VALUE - 1)", "MAX_VALUE");
         expectEqual(tester,
-                tester.multiply(tester.create((short) -2048), tester.create((short) -1)),
-                tester.create((short) -2048), "(-2048 * -1)", "-2048");
+                tester.multiply(tester.create((short) -1024), tester.create((short) -1)),
+                tester.create((short) -1024), "(-1024 * -1)", "-1024");
         expectEqual(tester,
                 tester.multiply(tester.create((short) 1193), tester.create((short) -1802)),
                 tester.create((short) 614), "(1193 * -1802)", "614");
