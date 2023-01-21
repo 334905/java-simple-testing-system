74b748a8656ff08e192b11782a064c4f28d34f83
Ivanov Timofei <334905@niuitmo.ru>
2023-01-21 09:19:45 +0300

    `checkMain` moved into `AbstractReverseTester`.

--- tests/reverse/AbstractReverseTester.java
+++ tests/reverse/AbstractReverseTester.java
@@ -139,4 +139,25 @@ public abstract class AbstractReverseTester extends MainTester<Void, int[][], in
                 }
         ).toArray(int[][]::new);
     }
+
+    @Override
+    protected boolean checkMain(final Void args, final int[][] input, final int[][] output) {
+        if (input.length != output.length) {
+            return false;
+        }
+        for (int i = 0; i < input.length; i++) {
+            int[] inputLine = input[i];
+            int[] outputLine = output[input.length - i - 1];
+
+            if (inputLine.length != outputLine.length) {
+                return false;
+            }
+            for (int j = 0; j < inputLine.length; j++) {
+                if (inputLine[j] != outputLine[inputLine.length - j - 1]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 }
--- tests/reverse/ReverseTester.java
+++ tests/reverse/ReverseTester.java
@@ -6,25 +6,4 @@ public class ReverseTester extends AbstractReverseTester {
     public ReverseTester(final ExtendedRandom random) throws ClassNotFoundException, NoSuchMethodException {
         super("Reverse", random);
     }
-
-    @Override
-    protected boolean checkMain(final Void args, final int[][] input, final int[][] output) {
-        if (input.length != output.length) {
-            return false;
-        }
-        for (int i = 0; i < input.length; i++) {
-            int[] inputLine = input[i];
-            int[] outputLine = output[input.length - i - 1];
-
-            if (inputLine.length != outputLine.length) {
-                return false;
-            }
-            for (int j = 0; j < inputLine.length; j++) {
-                if (inputLine[j] != outputLine[inputLine.length - j - 1]) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
 }
